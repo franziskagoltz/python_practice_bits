5d4401030ced9fd102bf00feea61159c0fe00639
Franziska von der Goltz <franziska@vdgoltz.eu>
2016-12-08 00:40:21 +0000

    initial solution for countrecursively

--- countrecursively.py
+++ countrecursively.py
@@ -14,6 +14,11 @@ For example:
 def count_recursively(lst):
     """Return number of items in a list, using recursion."""
 
+    if lst:
+        for num in lst:
+            return 1 + count_recursively(lst[1:])
+    return 0
+
 
 if __name__ == '__main__':
     import doctest
