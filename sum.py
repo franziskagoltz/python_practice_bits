9b09370ada119973e51dc18eed248fbcd9908fcc
Franziska von der Goltz <franziska@vdgoltz.eu>
2016-12-24 11:45:58 +0100

    solved with recursion, but can be optimized with less line of code and not updating 'total'

--- sum.py
+++ sum.py
@@ -24,6 +24,13 @@ The sum of an empty list is zero::
 def sum_list(nums):
     """Using recursion, return the sum of numbers in a list."""
 
+    total = 0
+
+    if nums:
+        return nums[0] + sum_list(nums[1:])
+
+    return total
+
 
 if __name__ == '__main__':
     import doctest
